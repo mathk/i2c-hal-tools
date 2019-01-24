82a910cd3d2a189c27b41a1d12ddfade666a0685
suen <matieu.suen@ville-ge.ch>
2019-01-24 16:04:44 +0100

    Adding auto increment i2c

--- src/autoincrement.rs
+++ src/autoincrement.rs
@@ -4,10 +4,75 @@ extern crate embedded_hal as hal;
 
 use core::mem;
 
+use {SerialWrite, SerialRead, Register};
 use hal::blocking::i2c::{Write, WriteRead};
-use cast::u16;
+use self::cast::{u16, u32};
 
-trait AutoIncrementI2c {
+pub struct AutoIncrementI2c;
+
+impl<T, R, E> SerialWrite<AutoIncrementI2c, R> for T
+where
+    T : Write<Error = E>,
+    R : Register,
+{
+
+    type Error = E;
+
+    fn write_u8(&mut self, addr: u8, reg: R, data: u8) -> Result<(), E> {
+        self.write(addr, &[reg.addr(), data])
+    }
+
+    fn write_le_u16(&mut self, addr: u8, reg: R, data: u16) -> Result<(), E> {
+        self.write(addr, &[reg.addr(), (data & 0xFF) as u8, (data >> 8) as u8])
+    }
+
+    fn write_be_u16(&mut self, addr: u8, reg: R, data: u16) -> Result<(), E> {
+        self.write(addr, &[reg.addr(), (data >> 8) as u8, (data & 0xFF) as u8])
+    }
+
+    fn write_le_u32(&mut self, addr: u8, reg: R, data: u32) -> Result<(), E> {
+        self.write(addr, &[reg.addr(), ((data >> 24) & 0xFF) as u8, ((data >> 16) & 0xFF) as u8, ((data >> 8) & 0xFF) as u8, (data & 0xFF) as u8])
+    }
+
+    fn write_be_u32(&mut self, addr: u8, reg: R, data: u32) -> Result<(), E> {
+        self.write(addr, &[reg.addr(), (data & 0xFF) as u8, ((data >> 8) & 0xFF) as u8, ((data >> 16) & 0xFF) as u8, ((data >> 24) & 0xFF) as u8])
+    }
 }
 
-impl<>
+impl<T, R, E> SerialRead<AutoIncrementI2c, R> for T
+where
+    T : WriteRead<Error = E>,
+    R : Register,
+{
+    type Error = E;
+
+    fn read_u8(&mut self, addr: u8, reg: R) -> Result<u8, E> {
+        let mut buffer: [u8; 1] = unsafe { mem::uninitialized() };
+        self.write_read(addr, &[reg.addr()], &mut buffer)?;
+        Ok(buffer[0])
+    }
+
+    fn read_le_u16(&mut self, addr: u8, reg: R) -> Result<u16, E> {
+        let mut buffer: [u8; 2] = unsafe { mem::uninitialized() };
+        self.write_read(addr, &[reg.addr()], &mut buffer)?;
+        Ok((u16(buffer[1]) << 8) & u16(buffer[0]))
+    }
+
+    fn read_be_u16(&mut self, addr: u8, reg: R) -> Result<u16, E> {
+        let mut buffer: [u8; 2] = unsafe { mem::uninitialized() };
+        self.write_read(addr, &[reg.addr()], &mut buffer)?;
+        Ok((u16(buffer[0]) << 8) & u16(buffer[1]))
+    }
+
+    fn read_le_u32(&mut self, addr: u8, reg: R) -> Result<u32, E> {
+        let mut buffer: [u8; 4] = unsafe { mem::uninitialized() };
+        self.write_read(addr, &[reg.addr()], &mut buffer)?;
+        Ok((u32(buffer[3]) << 24) & (u32(buffer[2]) << 16) & (u32(buffer[1]) << 8) & u32(buffer[0]))
+    }
+
+    fn read_be_u32(&mut self, addr: u8, reg: R) -> Result<u32, E> {
+        let mut buffer: [u8; 4] = unsafe { mem::uninitialized() };
+        self.write_read(addr, &[reg.addr()], &mut buffer)?;
+        Ok((u32(buffer[0]) << 24) & (u32(buffer[1]) << 16) & (u32(buffer[2]) << 8) & u32(buffer[3]))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,36 @@
+#![no_std]
 
-extern crate embedded-hal as hal;
+extern crate embedded_hal as hal;
 
 
-pub trait SerialWrite<Mode> {
+pub trait Register {
+    fn addr(&self) -> u8;
+}
+
+pub trait SerialWrite<Mode, R>
+where
+    R : Register
+{
     type Error;
 
-    fn write_u8(&mut self, addr: u8, data: u8) -> Result<(), Error>;
-    fn write_le_u16(&mut self, addr: u8, data: u16) -> Result<(), Error>;
-    fn write_be_u16(&mut self, addr: u8, data: u16) -> Result<(), Error>;
-    fn write_le_u32(&mut self, addr: u8, data: u32) -> Result<(), Error>;
-    fn write_be_u32(&mut self, addr: u8, data: u32) -> Result<(), Error>;
-    fn read_u8(&mut self, addr: u8) -> Result<u8, Error>;
-    fn read_le_u16(&mut self, addr: u8) -> Result<u16, Error>;
-    fn read_be_u16(&mut self, addr: u8) -> Result<u16, Error>;
-    fn read_le_u32(&mut self, addr: u8) -> Result<u16, Error>;
-    fn read_be_u32(&mut self, addr: u8) -> Result<u16, Error>;
+    fn write_u8(&mut self, addr: u8, reg: R, data: u8) -> Result<(), Self::Error>;
+    fn write_le_u16(&mut self, addr: u8, reg: R, data: u16) -> Result<(), Self::Error>;
+    fn write_be_u16(&mut self, addr: u8, reg: R, data: u16) -> Result<(), Self::Error>;
+    fn write_le_u32(&mut self, addr: u8, reg: R, data: u32) -> Result<(), Self::Error>;
+    fn write_be_u32(&mut self, addr: u8, reg: R, data: u32) -> Result<(), Self::Error>;
+}
+
+pub trait SerialRead<Mode, R>
+where
+    R : Register
+{
+    type Error;
+
+    fn read_u8(&mut self, addr: u8, reg: R) -> Result<u8, Self::Error>;
+    fn read_le_u16(&mut self, addr: u8, reg: R) -> Result<u16, Self::Error>;
+    fn read_be_u16(&mut self, addr: u8, reg: R) -> Result<u16, Self::Error>;
+    fn read_le_u32(&mut self, addr: u8, reg: R) -> Result<u32, Self::Error>;
+    fn read_be_u32(&mut self, addr: u8, reg: R) -> Result<u32, Self::Error>;
 }
 
 pub mod autoincrement;
